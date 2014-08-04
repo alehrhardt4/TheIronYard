06a63968c2ecf3a92dfc416b07c87c8e165acaac
Anna L. Ehrhardt <aehrhard@gmail.com>
2014-08-03 20:14:13 -0400

    blog

--- Homework/blog/directory.rb
+++ Homework/blog/directory.rb
@@ -7,13 +7,44 @@ DataMapper.setup(:default, 'mysql://root@localhost/posts')
 class Blog
 		include DataMapper::Resource
 		property :id, Serial
-		property :date, String
+		property :title, String
 		property :blog, String
 end
 
 DataMapper.finalize.auto_upgrade!
 
 get '/' do 	
-	erb :layout
+ @blog = Blog.all
+	erb :index, layout: :layout
 end
 
+get '/create_blog' do
+	erb :create_blog, layout: :layout
+end
+
+post '/create_blog' do
+	@blog = Blog.create params[:blog]
+	redirect to ('/')
+end
+
+get 'display_blog/:id' do
+	@blog = Blog.get params[:title]
+	erb :display_blog, layout: :layout
+end
+
+get 'edit_blog/:id' do
+	@blog = Blog.get params[:title]
+	erb :edit_blog, layout: :layout
+end
+
+get 'edit_blog/:id' do
+	@blog = Blog.get params[:title]
+	@blog.update params[:blog]
+	redirect to ('/')
+end
+
+delete '/delete_blog/:id' do
+	@blog = Blog.get params[:title]
+	@blog.destroy
+	redirect to('/')
+	end 
