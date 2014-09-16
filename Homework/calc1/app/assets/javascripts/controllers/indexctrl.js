b9b972b9cdb7f619ac44d1958e4bba4b05506192
Anna L. Ehrhardt <aehrhard@gmail.com>
2014-09-15 22:15:12 -0400

    updated angular app

--- Homework/calc1/app/assets/javascripts/controllers/indexctrl.js
+++ Homework/calc1/app/assets/javascripts/controllers/indexctrl.js
@@ -2,19 +2,19 @@ function CalcCtrl($scope){
 
 $scope.value = '0';
 $scope.display = '';
-$scope.clearValueIfNumkey = false;
-$scope.clearDisplayIfNumkey = false;
+$scope.clearValue = false;
+$scope.clearDisplay = false;
 
-var evaluate = function(){
+var calc = function(){
 return eval($scope.value).toString();
 }
-$scope.press = function(key){
+$scope.c = function(key){
 	if ("0123456789".indexOf(key) >= 0){
 
-		if ($scope.clearDisplayIfNumkey == true){
+		if ($scope.clearDisplay == true){
 		$scope.display = '';
 		}
-		if ($scope.clearValueIfNumkey  == true){
+		if ($scope.clearValue == true){
 		$scope.value = '0';
 		}
 
@@ -23,19 +23,19 @@ $scope.press = function(key){
 
 }
 
-	$scope.clearDisplayIfNumkey = false;
-	$scope.clearValueIfNumkey = false;
+	$scope.clearDisplay = false;
+	$scope.clearValue = false;
 
 		if ("*/+-".indexOf(key) >= 0){
 		$scope.value = evaluate() + key;
-		$scope.clearDisplayIfNumkey = true;
+		$scope.clearDisplay = true;
 		}
 
 		if (key == '='){
 		$scope.value = evaluate();
 		$scope.display = $scope.value;
-		$scope.clearDisplayIfNumkey = true;
-		$scope.clearValueIfNumkey = true;
+		$scope.clearDisplay = true;
+		$scope.clearValue = true;
 		}
 
 		if (key == 'C') {
